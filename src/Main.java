b613e02c2085a22a92547b9c3cf2f8e8fabc6e6b
Velimir <velimir.markovic90.gmail.com>
2019-01-24 20:09:07 +0100

    Inital commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World"); 
+        System.out.println("Velimir"); 
+        
+        String user = "Velimir Markovic";
+        System.out.println(user);
+        
+        Car audi = new Car();
+        audi.printVariables();
+     
+     
+      
+        
+        
+              
+    }
+    
+}
