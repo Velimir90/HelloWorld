8daeec725896271bacf1612d640a544ea9baec29
Velja <velja@Nemanjas-MacBook-Pro.local>
2019-01-29 00:24:39 +0100

    Izmenjeni stringovi u klasi Car

--- src/Car.java
+++ src/Car.java
@@ -14,12 +14,12 @@ public class Car {
     public void printVariables() {
         System.out.println("Maksimalna brzina: " + maxSpeed);
         System.out.println("Tezina: " + weight);
-        System.out.println(isTheCarOn);
-        System.out.println(model);
-        System.out.println(maxFuel);
-        System.out.println(currentFuel);
-        System.out.println(consumption);
-        System.out.println(mileagePassed);
+        System.out.println("Da li je auto ukljucen: " + isTheCarOn);
+        System.out.println("Model automobila: " + model);
+        System.out.println("Maksimalna zapremina rezervoara: " + maxFuel);
+        System.out.println("Trenutno stanje goriva: " + currentFuel);
+        System.out.println("Potrosnja goriva: " + consumption);
+        System.out.println("Predjena kilometraza: " + mileagePassed);
     }
     
     
